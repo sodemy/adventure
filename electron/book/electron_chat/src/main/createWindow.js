91c6650a7ca39658b1a5e225a0e014be419ae197
sodemy <waytohappinaissance@gmail.com>
2017-05-21 20:41:53 +0900

    adding react JSX hello app

---
+++ electron/book/electron_chat/src/main/createWindow.js
@@ -0,0 +1,13 @@
+'use strict'
+import {BrowserWindow} from "electron";
+
+let win;
+function createWindow() {
+    win = new BrowserWindow();
+    win.loadURL(`file://${__dirname}/../../index.html`);
+    win.on("close", () => {
+        win = null;
+    });
+}
+
+export default createWindow;
